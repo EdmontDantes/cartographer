2688b7af64220a6de9a20fec913d2621eabc8ac8
Bogdan Kowaltchook <bogdan.kowaltchook@codeimmersives.com>
2020-03-25 09:55:49 -0400

    4 failed, 20 passed, 24 total

--- main.js
+++ main.js
@@ -1,10 +1,61 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const triple = function(num) {
+  return num * 3;
+}
 
+const tripleAll = function(numbersArray) {
+  return numbersArray.map(triple);
+}
 
+const getNegativeValue = function(num) {
+  if (num < 0) {
+    return num;
+  } else if (num > 0) {
+    return num * (-1);
+  } else if (num === 0) {
+    return num;
+  }
+}
 
+const negativeValues = function (numbersArray) {
+  return numbersArray.map(getNegativeValue);
+}
 
+const makeQuestion = function (str) {
+  return str + '?';
+}
+
+const questionings = function (strArray) {
+  return strArray.map(makeQuestion);
+}
+
+const getInitials = function (str) {
+  
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ' ') {
+      return str[0] + str[i + 1];
+    }
+  }
+}
+
+const changeToInitials = function (strArray) {
+
+  return strArray.map(getInitials);
+}
+
+const doubleIfEven = function (num) {
+  if (num % 2 === 0) {
+    return num * 2;
+  } else {
+    return num;
+  }
+}
+
+const doubleEven = function (numbersArray) {
+  return numbersArray.map(doubleIfEven);
+}
 /********************************
  * OUR CODE HERE - DON'T TOUCH! *
  ********************************/
